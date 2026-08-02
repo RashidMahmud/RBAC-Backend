7a25d93bb9f2d9cb5de7b8ff4e5d06c5067bbaf6
RashidMahmud <rashidmahmud999@gmail.com>
2026-08-03 02:05:42 +0600

    Refactor app and server initialization: improve formatting and ensure proper Prisma client import

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,12 @@ import config from "./config";
 
 const app: Application = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: config.app_url,
     credentials: true,
-}));
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
