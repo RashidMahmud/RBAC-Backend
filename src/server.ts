7414eb4513e040f122b4333b64fdd090e1705571
RashidMahmud <rashidmahmud999@gmail.com>
2026-08-02 21:44:52 +0600

    server testing

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,12 @@ const PORT = process.env.PORT || 5000;
 async function main() {
   try {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+  }
 }
 
 main();
