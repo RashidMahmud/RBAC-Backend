a0064a550a3631fd54a0ba35503160b9cb254039
RashidMahmud <rashidmahmud999@gmail.com>
2026-08-04 20:35:52 +0600

    set user.controller.ts file

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,7 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
-import httpStatus from "http-status";
-import { prisma } from "./lib/prisma";
-import bcrypt from "bcryptjs";
+import { userRoutes } from "./modules/user/user.route";
 
 const app: Application = express();
 
@@ -22,6 +20,7 @@ app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
-app.post();
+
+app.use("/api/user", userRoutes);
 
 export default app;
