ad5262ac13160d8c901d961c4a3434a2d439e62d
RashidMahmud <rashidmahmud999@gmail.com>
2026-08-06 21:17:03 +0600

    Refactor user registration response handling to use sendResponse utility

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -28,12 +28,45 @@ import { catchAsync } from "../../utils/catchAsync";
 //   }
 // };
 
+type TMeta = {
+  page: number;
+  limit: number;
+  total: number;
+};
+
+type TResponseData<T> = {
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: T;
+  meta?: TMeta;
+};
+
+const sendResponse = <T>(res: Response, data: TResponseData<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    statusCode: data.statusCode,
+    message: data.message,
+    data: data.data,
+    meta: data.meta,
+  });
+};
+
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
     const user = await userService.registerUserIntoDB(payload);
 
-    res.status(httpStatus.CREATED).json({
+    // res.status(httpStatus.CREATED).json({
+    //   success: true,
+    //   statusCode: httpStatus.CREATED,
+    //   message: "User registered successfully",
+    //   data: {
+    //     user,
+    //   },
+    // });
+
+    sendResponse(res, {
       success: true,
       statusCode: httpStatus.CREATED,
       message: "User registered successfully",
