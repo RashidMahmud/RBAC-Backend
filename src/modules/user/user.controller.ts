02b07c1d2ef82529ae845b9f806821cbcf5a4864
RashidMahmud <rashidmahmud999@gmail.com>
2026-08-09 21:10:01 +0600

    Refactor token verification to use jwtUtils in getMyProfile endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,6 +5,7 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import jwt from "jsonwebtoken";
 import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -26,7 +27,7 @@ const getMyProfile = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { accessToken } = req.cookies;
 
-    const verifiedToken = jwt.verify(accessToken, config.jwt_access_secret);
+    const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret)
 
     console.log(verifiedToken);
 
