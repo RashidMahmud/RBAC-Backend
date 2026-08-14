b4dd42e1f93aec77e5d6fcc68f2f12aafe52e215
RashidMahmud <rashidmahmud999@gmail.com>
2026-08-15 01:16:58 +0600

    Implement auth middleware for role-based access control and user validation

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,76 +1,12 @@
 import { NextFunction, Router, Request, Response } from "express";
 import { userController } from "./user.controller";
-import { jwtUtils } from "../../utils/jwt";
-import config from "../../config";
 import { Role } from "../../../generated/prisma/enums";
-import httpStatus from "http-status";
-import { catchAsync } from "../../utils/catchAsync";
-import { JwtPayload } from "jsonwebtoken";
-import { prisma } from "../../lib/prisma";
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        email: string;
-        name: string;
-        id: string;
-        role: Role;
-      };
-    }
-  }
-}
+import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
-const auth = (...requiredRoles: Role[]) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.accessToken;
-    // || req.headers.authorization?.startsWith("Bearer")
-    //   ? req.headers.authorization?.split(" ")[1]
-    //   : req.headers.authorization;
 
-    if (!token) {
-      throw new Error(
-        "You are not logged in. Please log in to access this resource.",
-      );
-    }
-    const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
-
-    if (!verifiedToken.success) {
-      throw new Error(verifiedToken.error);
-    }
-    const { email, name, id, role } = verifiedToken.data as JwtPayload;
-
-    if (requiredRoles.length && !requiredRoles.includes(role)) {
-      throw new Error(
-        "Forbidden. You don't have permission to access this resource. ",
-      );
-    }
-    const user = await prisma.user.findUnique({
-      where: {
-        id,
-        email,
-        name,
-        role,
-      },
-    });
-    if (!user) {
-      throw new Error("User not found. Please log in again.");
-    }
-    if (user.activeStatus === "BLOCKED") {
-      throw new Error("Your account has been blocked. Please contact support.");
-    }
-    req.user = {
-      email,
-      name,
-      id,
-      role,
-    };
-    next();
-  });
-};
 router.get(
   "/me",
   // (req: Request, res: Response, next: NextFunction) => {
