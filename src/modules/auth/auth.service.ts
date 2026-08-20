fa612a4db91bd34ddec2920d5a1bf90d35526d88
RashidMahmud <rashidmahmud999@gmail.com>
2026-08-20 20:16:25 +0600

    Refactor refreshToken function for improved error handling and readability

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -47,7 +47,36 @@ const loginUser = async (payload: ILoginUser) => {
 };
 
 const refreshToken = async (refreshToken: string) => {
-  const verifiedRefreshToken = jwtUtils.verifyToken(refreshToken, config.jwt_refresh_secret)
+  const verifiedRefreshToken = jwtUtils.verifyToken(
+    refreshToken,
+    config.jwt_refresh_secret,
+  );
+
+  if (!verifiedRefreshToken.success) {
+    throw new Error(verifiedRefreshToken.error);
+  }
+
+  const {id} = verifiedRefreshToken
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {id}
+  })
+
+  if(user.activeStatus === "BLOCKED") {
+    throw new Error("User is blocked!")
+  }
+  const jwtPayload = {
+    id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  }
+
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions
+  )
 };
 
 export const authService = {
